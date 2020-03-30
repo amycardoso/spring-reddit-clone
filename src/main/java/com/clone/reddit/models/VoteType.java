dcedf943214fbde66e630b16dc5d00526d0b34d7
Amélia Alice <alice.cardoso10@gmail.com>
2020-03-30 09:30:07 -0300

    Create VoteType

---
+++ src/main/java/com/clone/reddit/models/VoteType.java
@@ -0,0 +1,26 @@
+package com.clone.reddit.models;
+
+import java.util.Arrays;
+
+import com.clone.reddit.exception.SpringRedditException;
+
+public enum VoteType {
+    UPVOTE(1), DOWNVOTE(-1),
+    ;
+
+    private int direction;
+
+    VoteType(int direction) {
+    }
+
+    public static VoteType lookup(Integer direction) {
+        return Arrays.stream(VoteType.values())
+                .filter(value -> value.getDirection().equals(direction))
+                .findAny()
+                .orElseThrow(() -> new SpringRedditException("Vote not found"));
+    }
+
+    public Integer getDirection() {
+        return direction;
+    }
+}
