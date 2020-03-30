09caae38aa9202ac5ca14f244da8e71970211ac6
Amélia Alice <alice.cardoso10@gmail.com>
2020-03-30 09:47:57 -0300

    Update Post API with Vote

--- src/main/java/com/clone/reddit/dto/PostResponse.java
+++ src/main/java/com/clone/reddit/dto/PostResponse.java
@@ -14,4 +14,7 @@ public class PostResponse {
     private String description;
     private String userName;
     private String subredditName;
+    private Integer voteCount;
+    private Integer commentCount;
+    private String duration;
 }
--- src/main/java/com/clone/reddit/mapper/PostMapper.java
+++ src/main/java/com/clone/reddit/mapper/PostMapper.java
@@ -5,23 +5,42 @@ import com.clone.reddit.dto.PostResponse;
 import com.clone.reddit.models.Post;
 import com.clone.reddit.models.Subreddit;
 import com.clone.reddit.models.User;
+import com.clone.reddit.repository.CommentRepository;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    // @Autowired
+    // private VoteRepository voteRepository;
+    // @Autowired
+    // private AuthService authService;
+
+
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
-    @Mapping(target = "subreddit", source = "subreddit")
-    @Mapping(target = "user", source = "user")
     @Mapping(target = "description", source = "postRequest.description")
-    Post map(PostRequest postRequest, Subreddit subreddit, User user);
- 
+    @Mapping(target = "subreddit", source = "subreddit")
+    @Mapping(target = "voteCount", constant = "0")
+    public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
+
     @Mapping(target = "id", source = "postId")
-    @Mapping(target = "postName", source = "postName")
-    @Mapping(target = "description", source = "description")
-    @Mapping(target = "url", source = "url")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
